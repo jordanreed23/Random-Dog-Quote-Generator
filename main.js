7904d513093c733c8b17e17868288c9307f0f669
andfwas <andfwas@gmail.com>
2017-09-01 15:09:33 -0600

    first semicomplete project

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var form = document.getElementById('pickBreed')
-var dogPic = document.getElementById('dogPic')
+var dogPic = document.getElementsByClassName('dogPic')[0]
+
 
 form.addEventListener('submit', function(event) {
+  dogPic.innerHTML = ''
   event.preventDefault()
+  var div = document.createElement('div')
   // clearData()
   var breed = event.target.elements.breed.value
   fetch('https://dog.ceo/api/breed/' + breed + "/images")
@@ -14,9 +17,24 @@ form.addEventListener('submit', function(event) {
           var dogLength = dogImages.message.length
             var randomDog = Math.floor(Math.random() * dogLength)
             var randomImg = dogImages.message[randomDog]
-            var img = document.createElement('img')
-            img.src = randomImg
-            dogPic.append(img)
+            div.style.backgroundImage = 'url(' + randomImg + ')'
+            // div.style.backgroundRepeat = 'no-repeat'
+            div.style.backgroundSize = 'cover'
+            div.setAttribute('class', 'puppyPic')
+            // img.setAttribute('class', 'puppyPic')
+            dogPic.append(div)
         })
+    })
+    fetch("https://talaikis.com/api/quotes/random/")
+    .then(function(response) {
+      return response.json()
+      .then(function(quotes) {
+        var quote = quotes.quote
+        var author = quotes.author
+        var p = document.createElement('p')
+        var authorChange = author.replace(' ', ' \"' + breed + '\" ')
+        p.innerText = quote + '\n\n' + "-" + authorChange
+        div.append(p)
+      })
     })
 })
