633c57674ec9bf4cfd695d2b13b658d9424ac819
andfwas <andfwas@gmail.com>
2017-09-01 12:00:08 -0600

    form and submit working, randomDog working, random image going to the page, images pile up

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+var form = document.getElementById('pickBreed')
+var dogPic = document.getElementById('dogPic')
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault()
+  // clearData()
+  var breed = event.target.elements.breed.value
+  fetch('https://dog.ceo/api/breed/' + breed + "/images")
+    .then(function(response) {
+      return response.json()
+
+        .then(function(dogImages) {
+          // console.log(dogImages.message[0])
+          var dogLength = dogImages.message.length
+            var randomDog = Math.floor(Math.random() * dogLength)
+            var randomImg = dogImages.message[randomDog]
+            var img = document.createElement('img')
+            img.src = randomImg
+            dogPic.append(img)
+        })
+    })
+})
